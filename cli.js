2b73b07d3c6abacef6d153377e0b442a022d9b10
Serge <iefserge@runtimejs.org>
2014-11-26 00:00:53 +0200

    Set cross compiler PATH before build

--- cli.js
+++ cli.js
@@ -9,7 +9,8 @@ var error = require('./error');
 var config = require('./config');
 var shellexec = require('./shellexec');
 var exec = require('./exec');
-var tilde = require('tilde-expansion');
+var resolvePath = require('./path').resolvePath;
+var addPathEnv = require('./path').addPathEnv;
 
 function usage() {
   shell.echo('USAGE: runtime [command]');
@@ -65,22 +66,24 @@ if ('editconfig' === command) {
 
 var conf = config.parse();
 var dockerPrefix = 'docker run --rm -w /mnt -v $(pwd):/mnt:rw runtimejs ';
+var runtimePath = resolvePath(conf.RuntimePath);
+var crossCompilerPath = resolvePath(conf.CrossCompilerPath);
 
 function build(cb) {
+  addPathEnv(pathUtils.resolve(crossCompilerPath, 'bin'));
+
   var cmd = 'scons';
   if (argv.docker) {
     cmd = dockerPrefix + cmd;
   }
 
-  tilde(conf.RuntimePath, function(runtimePath) {
-    shellexec('cd ' + runtimePath + ' && ' + cmd, function(code, output) {
-      if (0 !== code) {
-        return cb(new Error('build failed'));
-      }
+  shellexec('cd ' + runtimePath + ' && ' + cmd, function(code, output) {
+    if (0 !== code) {
+      return cb(new Error('build failed'));
+    }
 
-      shell.echo(' --- build ok --- '.green);
-      return cb(null);
-    });
+    shell.echo(' --- build ok --- '.green);
+    return cb(null);
   });
 }
 
@@ -90,15 +93,13 @@ function initrd(cb) {
     cmd = dockerPrefix + cmd;
   }
 
-  tilde(conf.RuntimePath, function(runtimePath) {
-    shellexec('cd ' + runtimePath + ' && ' + cmd, function(code, output) {
-      if (0 !== code) {
-        return cb(new Error('initrd failed'));
-      }
+  shellexec('cd ' + runtimePath + ' && ' + cmd, function(code, output) {
+    if (0 !== code) {
+      return cb(new Error('initrd failed'));
+    }
 
-      shell.echo(' --- initrd ok --- '.green);
-      return cb(null);
-    });
+    shell.echo(' --- initrd ok --- '.green);
+    return cb(null);
   });
 }
 
@@ -108,43 +109,45 @@ function start(cb) {
     return error('error: qemu is not installed (not found qemu-system-x86_64)');
   }
 
-  tilde(conf.RuntimePath, function(runtimePath) {
-    if (!shell.test('-d', runtimePath) || !shell.test('-f', pathUtils.resolve(runtimePath, 'SConstruct'))) {
-      return error('error: could not locate runtime directory at "' + runtimePath + '", check config file');
-    }
+  if (!shell.test('-d', runtimePath) || !shell.test('-f', pathUtils.resolve(runtimePath, 'SConstruct'))) {
+    return error('error: could not locate runtime directory at "' + runtimePath + '", check config file');
+  }
 
-    var a = [
-      '-m 512',
-      '-smp 1',
-      '-s',
-      '-kernel ' + pathUtils.resolve(runtimePath, 'disk/boot/runtime'),
-      '-initrd ' + pathUtils.resolve(runtimePath, 'disk/boot/initrd'),
-    ];
+  var a = [
+    '-m 512',
+    '-smp 1',
+    '-s',
+    '-kernel ' + pathUtils.resolve(runtimePath, 'disk/boot/runtime'),
+    '-initrd ' + pathUtils.resolve(runtimePath, 'disk/boot/initrd'),
+  ];
 
-    if (argv.net) {
-      a.push('-net nic,model=virtio,macaddr=1a:46:0b:ca:bc:7c');
-      a.push('-net user,net=192.168.76.0/24,dhcpstart=192.168.76.9,hostfwd=udp::9000-:9000,hostfwd=tcp::9000-:9000');
-    }
+  if (argv.net) {
+    a.push('-net nic,model=virtio,macaddr=1a:46:0b:ca:bc:7c');
+    a.push('-net user,net=192.168.76.0/24,dhcpstart=192.168.76.9,hostfwd=udp::9000-:9000,hostfwd=tcp::9000-:9000');
+  }
 
-    if (argv.netdump) {
-      a.push('-net dump,file=vm.pcap');
-    }
+  if (argv.netdump) {
+    a.push('-net dump,file=vm.pcap');
+  }
 
-    if (argv.kvm) {
-      a.push('-enable-kvm');
-      a.push('-no-kvm-irqchip');
-    }
+  if (argv.kvm) {
+    a.push('-enable-kvm');
+    a.push('-no-kvm-irqchip');
+  }
 
-    if (argv.curses) {
-      a.push('-curses');
-      a.push('-serial file:serial.txt');
-    } else {
-      a.push('-serial stdio');
-    }
+  if (argv.curses) {
+    a.push('-curses');
+    a.push('-serial file:serial.txt');
+  } else {
+    a.push('-serial stdio');
+  }
 
-    shell.echo(' --- starting qemu --- '.green);
+  shell.echo(' --- starting qemu --- '.green);
+  if (argv.curses) {
     exec(qemu, a.join(' ').split(' '), cb);
-  });
+  } else {
+    shellexec(qemu + ' ' + a.join(' '));
+  }
 }
 
 switch (command) {
---
+++ path.js
@@ -0,0 +1,21 @@
+var pathUtils = require('path');
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
+module.exports = {
+  resolvePath: function(path) {
+    return String(path).split('${HOME}').join(getUserHome());
+  },
+  addPathEnv: function(addPath) {
+    var paths = process.env.PATH.split(pathUtils.delimiter);
+    if (paths.indexOf(addPath) > -1) {
+      return;
+    }
+    console.log(paths);
+
+    paths.unshift(addPath);
+    process.env.PATH = paths.join(pathUtils.delimiter);
+  }
+};
